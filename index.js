743c6447cb5aee66a86abb7905e57cf0f8172afa
Jens Hausdorf <mail@jens-hausdorf.de>
2018-10-20 20:34:42 +0200

    add(connection): Add route lookup

--- index.js
+++ index.js
@@ -22,6 +22,24 @@ app.get('/locations', function (request, response) {
     });
 });
 
+app.get('/connections', (req, res) => {
+    let sql = `SELECT
+    hs1.name AS hs1_name, hs1.latitude AS hs1_latitude, hs1.longitude AS hs1_longitude,
+    hs2.name AS hs2_name, hs2.latitude AS hs2_latitude, hs2.longitude AS hs2_longitude,
+    connections.*, user.user_name
+    FROM connections
+    LEFT JOIN rides ON rides.connection_id = connections.connection_id
+    LEFT JOIN user ON user.user_id = connections.user_id
+    LEFT JOIN hackerspaces AS hs1 ON hs1.hackerspace_id = rides.start_location_id
+    LEFT JOIN hackerspaces AS hs2 ON hs2.hackerspace_id = rides.destination_location_id;
+    `;
+    database.getDatabase().query(sql, (err, results) => {
+        if (err) throw err;
+
+        res.json(results);
+    });
+});
+
 // only serve the app as soon as we have database access.
 database.establishConnection().then(function () {
     app.listen(8080, "0.0.0.0");
